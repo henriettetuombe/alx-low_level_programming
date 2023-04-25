ebcb6d0262544369c6c05a363355d3bdd29890eb
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-04-25 06:32:08 +0000

    function nessted loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet in lowercase followed by a new line
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+
+i = 0;
+
+while (i < 10)
+{
+ch = 'a';
+
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+
+_putchar('\n');
+i++;
+}
+
+}
