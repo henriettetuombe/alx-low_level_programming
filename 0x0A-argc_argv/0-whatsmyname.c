1a459bd485f6e2c9554c6cda61d33f83853e1902
henriettetuombe <116841112+henriettetuombe@users.noreply.github.com>
2023-05-12 11:38:57 +0000

    args argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
